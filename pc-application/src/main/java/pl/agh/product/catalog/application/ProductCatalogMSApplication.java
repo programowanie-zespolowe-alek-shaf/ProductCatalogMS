768dcde0f3d6a4a8e5eb65d253536927303ae697
rxrt8 <rxrt8@users.noreply.github.com>
2020-04-29 22:02:42 +0200

    add eureka client (#2)

--- pc-application/src/main/java/pl/agh/product/catalog/application/ProductCatalogMSApplication.java
+++ pc-application/src/main/java/pl/agh/product/catalog/application/ProductCatalogMSApplication.java
@@ -3,6 +3,7 @@ package pl.agh.product.catalog.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 )
 @EntityScan("pl.agh.product.catalog")
 @EnableJpaRepositories("pl.agh.product.catalog")
+@EnableEurekaClient
 public class ProductCatalogMSApplication {
 
     public static void main(String[] args) {
